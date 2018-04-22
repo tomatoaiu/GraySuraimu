996dc7d09747b3523e24da52dd4b9e527f635cc5
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2018-04-22 22:43:27 +0900

    feat: status dom

--- src/components/container.vue
+++ src/components/container.vue
@@ -1,6 +1,27 @@
 <template>
   <main>
-    {{word}}
+    <div class="row justify-content-md-center">
+      <div>
+        TODO enemy
+      </div>
+    </div>
+    <div class="row justify-content-md-center">
+      <div>
+        <b-card-group deck>
+          <b-card 
+            v-for="({title, src, text}, index) of characters"
+            :key="index"
+            :title="title"
+            :img-src="src"
+            img-alt="Img"
+            img-top>
+            <p class="card-text">
+                {{ text }}
+            </p>
+          </b-card>
+        </b-card-group>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -9,9 +30,24 @@ export default {
   name: 'container',
   data() {
     return {
-      word: "Hello, world"
+      characters: [
+        {
+          title : 'usu',
+          src: 'https://lorempixel.com/200/200/',
+          text: "text text text"
+        },
+        {
+          title : 'sharu',
+          src: 'https://lorempixel.com/200/200/',
+          text: "text text text"
+        },
+        {
+          title : 'hachi',
+          src: 'https://lorempixel.com/200/200/',
+          text: "text text text"
+        }
+      ]
     }
   }
 }
 </script>
-
